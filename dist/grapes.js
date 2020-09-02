d591677d9a6599d28a767ee4f37347c6f06d38b3
Rafael Mejía <rafael.mejia@positiongreen.com>
2020-09-02 15:16:22 +0100

    package-lock.json

--- dist/grapes.js
+++ dist/grapes.js
@@ -39439,7 +39439,7 @@ module.exports = function () {
     plugins: plugins,
 
     // Will be replaced on build
-    version: '0.14.65',
+    version: '0.14.66',
 
     /**
      * Initialize the editor with passed options
